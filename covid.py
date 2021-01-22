17e5806a207a34c2c929ed2ee31250e7d1843328
Áron Széles <szelesaron39@]mail.com>
2021-01-22 14:00:03 +0000

    Added extra data cleaning step

--- covid.py
+++ covid.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def get_days_left(country):
     #Getting the data - cleaning
+    #country = "Malta"
+    
     df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
     
     vacc_col = [vac for vac in df.columns if "vaccin" in vac]
@@ -14,18 +16,21 @@ def get_days_left(country):
     
     #dealing with missing values
     df= df.fillna(method = "ffill")
+    df= df.fillna(method = "bfill")
     
     #Setting up values - getting population from vaccination / 100 and sum(vaccinated)
     try:
         population = df["total_vaccinations"].iloc[-1] / (df["total_vaccinations_per_hundred"].iloc[-1] / 100)
         
         not_vaccinated = population - df["total_vaccinations"].iloc[-1]
-        seven_day_average = sum(df["new_vaccinations"].iloc[-7:]) / 7
+        seven_day_average = sum(df["new_vaccinations"].iloc[-14:]) / 14
         
-        days_left = int((not_vaccinated / 2) / seven_day_average)
+        days_left = (not_vaccinated / 2) / seven_day_average
         return days_left
     
     except:
         print("Country does not provide enough data.")
         
-get_days_left("Germany")
+get_days_left("Malta")
+
+
