fead84044c56e49230804b8c7b16d47e6a432c7b
Áron Széles <szelesaron39@]mail.com>
2021-01-25 12:52:48 +0000

    fix

--- covid.py
+++ covid.py
@@ -17,6 +17,7 @@ def get_countries():
 #NEEDS TO BE OPTIMIZED
 def get_days_left(country):
     #Getting the data - cleaning
+#    country = "Hungary"
     df = pd.read_csv(url)
     
     df = df[df["location"] == country]._get_numeric_data()
