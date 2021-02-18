2c64096e6121112e69561034b22e36b1337a413f
Áron Széles <szelesaron39@gmail.com>
2021-02-18 16:44:10 +0000

    renamed filerenamed filess

---
+++ update.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import sqlite3
+import numpy as np
+import os
+
+os.chdir(r"C:\Users\Áron\Desktop\covid\herd-immunity")
+
+url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
+
+
+#building list of countires who are vaccinating
+def get_countries():
+    df = pd.read_csv(url)
+    countries = df[ df["total_vaccinations"] > 0]["location"].unique().tolist()
+    return countries
+    
+#NEEDS TO BE OPTIMIZED
+def get_days_left(country):
+    #Getting the data - cleaning
+#    country = "Hungary"
+    df = pd.read_csv(url)
+    
+    df = df[df["location"] == country]._get_numeric_data()
+    
+    mask = np.all(np.isnan(df), axis=1) | np.all(df == 0, axis=1)
+    df = df[~mask]
+    
+    #dealing with missing values
+    df= df.fillna(method = "ffill")
+    
+    #Setting up values - getting population from vaccination / 100 and sum(vaccinated)
+    try:
+        population = df["total_vaccinations"].iloc[-1] / (df["total_vaccinations_per_hundred"].iloc[-1] / 100)
+        
+        not_vaccinated = population - df["people_fully_vaccinated"].iloc[-1]
+        seven_day_average = sum(df["daily_vaccinations"].iloc[-14:]) / 14
+        
+        days_left = (not_vaccinated *0.7) / seven_day_average
+        
+        #this return format allows to be inserted into a dataframe
+        return {'Country': country,
+                'Days': days_left}
+    except:
+        return 0
+
+
+
+#generating dataframe to return - getting every countries' immunity date
+def build_list():
+    l = []    
+    countries = get_countries()
+    for country in countries:
+        l.append(get_days_left(country))
+    
+    return l
+
+
+#Database things
+def insert_database(df):
+    #creating database
+    conn = sqlite3.connect("covid.db")
+    c = conn.cursor()
+    
+    #creating table
+    c.execute('CREATE TABLE IF NOT EXISTS ImmunityDate  (Country text, Days number)')
+    conn.commit()
+    
+    #sending df to db
+    df.to_sql('ImmunityDate', conn, if_exists='replace', index = False)
+
+
+
+
+if __name__ == '__main__':
+    #putting it into a df
+    days_left_df = pd.DataFrame(build_list()).dropna().reset_index(drop=True)
+    days_left_df["Days"] = days_left_df["Days"].astype(int)
+    insert_database(days_left_df)
+    
+
+
+
+
+
